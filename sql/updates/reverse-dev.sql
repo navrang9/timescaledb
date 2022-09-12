e967c8546510dae755f1a64e17cef1d78378013c
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2022-09-12 16:26:55 -0300

    Change parameter names of cagg_migrate procedure
    Removed the underline character prefix '_' from the parameter names of
    the procedure `cagg_migrate`. The new signature is:
    
    cagg_migrate(
        IN cagg regclass,
        IN override boolean DEFAULT false,
        IN drop_old boolean DEFAULT false
    )

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,17 +1,16 @@
 
 -- gapfill with timezone support
-CREATE FUNCTION @extschema@.time_bucket_gapfill(bucket_width INTERVAL, ts TIMESTAMPTZ, timezone TEXT, start TIMESTAMPTZ=NULL, finish TIMESTAMPTZ=NULL) RETURNS TIMESTAMPTZ
-AS '@MODULE_PATHNAME@', 'ts_gapfill_timestamptz_timezone_bucket' LANGUAGE C VOLATILE PARALLEL SAFE;
+DROP FUNCTION @extschema@.time_bucket_gapfill(INTERVAL,TIMESTAMPTZ,TEXT,TIMESTAMPTZ,TIMESTAMPTZ);
 
 ALTER TABLE _timescaledb_catalog.compression_chunk_size DROP CONSTRAINT compression_chunk_size_pkey;
-ALTER TABLE _timescaledb_catalog.compression_chunk_size ADD CONSTRAINT compression_chunk_size_pkey PRIMARY KEY(chunk_id);
+ALTER TABLE _timescaledb_catalog.compression_chunk_size ADD CONSTRAINT compression_chunk_size_pkey PRIMARY KEY(chunk_id,compressed_chunk_id);
 
-DROP PROCEDURE IF EXISTS @extschema@.cagg_migrate (REGCLASS, BOOLEAN, BOOLEAN);
+DROP PROCEDURE @extschema@.cagg_migrate (REGCLASS, BOOLEAN, BOOLEAN);
 
 CREATE PROCEDURE @extschema@.cagg_migrate (
-    cagg REGCLASS,
-    override BOOLEAN DEFAULT FALSE,
-    drop_old BOOLEAN DEFAULT FALSE
+    _cagg REGCLASS,
+    _override BOOLEAN DEFAULT FALSE,
+    _drop_old BOOLEAN DEFAULT FALSE
 )
 LANGUAGE plpgsql AS
 $BODY$
@@ -25,7 +24,7 @@ BEGIN
     INTO _cagg_schema, _cagg_name
     FROM pg_catalog.pg_class
     JOIN pg_catalog.pg_namespace ON pg_namespace.oid OPERATOR(pg_catalog.=) pg_class.relnamespace
-    WHERE pg_class.oid OPERATOR(pg_catalog.=) cagg::pg_catalog.oid;
+    WHERE pg_class.oid OPERATOR(pg_catalog.=) _cagg::pg_catalog.oid;
 
     -- maximum size of an identifier in Postgres is 63 characters, se we need to left space for '_new'
     _cagg_name_new := pg_catalog.format('%s_new', pg_catalog.substr(_cagg_name, 1, 59));
@@ -34,7 +33,7 @@ BEGIN
     _cagg_data := _timescaledb_internal.cagg_migrate_pre_validation(_cagg_schema, _cagg_name, _cagg_name_new);
 
     -- create new migration plan
-    CALL _timescaledb_internal.cagg_migrate_create_plan(_cagg_data, _cagg_name_new, override, drop_old);
+    CALL _timescaledb_internal.cagg_migrate_create_plan(_cagg_data, _cagg_name_new, _override, _drop_old);
     COMMIT;
 
     -- execute the migration plan
